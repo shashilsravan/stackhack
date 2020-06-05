179af6d42a56358fab58a9684b5af8c59c16d49b
sravan <shashilsravan.ss.ss@gmail.com>
2020-06-05 17:58:12 +0530

    'Tired1'

--- django_project/settings.py
+++ django_project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'e8bf99c1345678a041e1370aea79deb224f9901917baeaa9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'sstodoplanner.herokuapp.com']
 
