cdbaffe3c21fa3790159f2bb3ff752db398d896b
sravan <shashilsravan.ss.ss@gmail.com>
2020-06-05 17:51:46 +0530

    'Tired'

--- django_project/settings.py
+++ django_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -148,3 +149,5 @@ EMAIL_PORT='587'
 EMAIL_USE_TLS=True
 EMAIL_HOST_USER='sravanshashil@gmail.com'
 EMAIL_HOST_PASSWORD='sravan777'
+
+django_heroku.settings(locals())
